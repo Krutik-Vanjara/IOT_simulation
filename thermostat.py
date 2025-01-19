999b7d4f053fcb1fbe9ca98610b86c08a90a4731
Abhishek Gupta <abhishekg17052002@gmail.com>
2025-01-19 17:51:43 +0100

    Update thermostat.py

--- thermostat.py
+++ thermostat.py
@@ -1,7 +1,19 @@
 class Thermostat:
-    def __init__(self, temperature=22):
-        self.temperature = temperature  # Default is 22°C
+    def _init_(self):
+        self.status = "OFF"
+        self.current_temperature = 20
+        self.target_temperature = None
 
-    def set_temperature(self, temperature):
-        self.temperature = temperature
-        print(f"Temperature set to {self.temperature}°C.")
+    def toggle_status(self):
+        if self.status == "OFF":
+            self.status = "ON"
+        else:
+            self.status = "OFF"
+        print(f"Thermostat is now {self.status}")
+
+    def set_temperature(self, target):
+        if 16 <= target <= 30:
+            self.target_temperature = target
+            print(f"Target temperature set to {target}°C")
+        else:
+            print("Error: Temperature must be between 16°C and 30°C.")
